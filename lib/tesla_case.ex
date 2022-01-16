416ecb24368194a46465feeec08f3d4edbbb902e
Wígny Almeida <wignybora@gmail.com>
2022-01-16 17:20:53 +0000

    feat: initial commit

---
+++ lib/tesla_case.ex
@@ -0,0 +1,18 @@
+defmodule TeslaCase do
+  @moduledoc """
+  Documentation for `TeslaCase`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TeslaCase.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tesla_case_test.exs
@@ -0,0 +1,8 @@
+defmodule TeslaCaseTest do
+  use ExUnit.Case
+  doctest TeslaCase
+
+  test "greets the world" do
+    assert TeslaCase.hello() == :world
+  end
+end
