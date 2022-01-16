68e6d32f38efa26839f66cb813f23f6b80b941b0
Wígny Almeida <wignybora@gmail.com>
2022-01-16 20:38:22 +0000

    chore: add `ex_doc` dep

--- mix.exs
+++ mix.exs
@@ -7,22 +7,34 @@ defmodule TeslaCase.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:tesla, "~> 1.0"},
-      {:recase, "~> 0.7.0"}
+      {:recase, "~> 0.7.0"},
+      {:ex_doc, "~> 0.27.3", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Tesla middleware for converting the body keys of the request and response"
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/wigny/tesla_case"}
     ]
   end
 end
